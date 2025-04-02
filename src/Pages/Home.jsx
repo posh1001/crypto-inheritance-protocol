8082be7be756f2cf1222486ec27ab2fa4cc1dcee
posh1001 <orifepaul30@gmail.com>
2025-04-02 03:38:13 +0100

    Finished with the header

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -10,7 +10,6 @@ const Home = () => {
      <HomeNavbar/>
      <div className="header"> 
       <div className="container">
-        <div className="row">
           <div className="col-lg col-md">
              <div className="header-text">
              <h3>Your legacy deserves a secure tomorrow.</h3>
@@ -23,11 +22,9 @@ const Home = () => {
                   </Button>
             </Link>
           </div>
-        </div>
       </div>
       <br />
       <br />
-      </div>
       <section id='sec1'>
         <div className="container">
           <div className="row"> 
@@ -41,15 +38,59 @@ const Home = () => {
             </p>
           </div>
         </div>
-        <div className="col-lg-4 col-md-4">
-           <div className="sec1-img">
+        </div>
+       <br />
+       <div className="row">
+         <div className="d-flex justify-content-between">
+       <div className="col-lg-4 col-md-4">
+           <div className="sec1-img py-5">
              <img src={Shield} alt="shield-photo" style={{width: "1050px"}} />
            </div>
         </div>
-
-        <div className="col-lg-8"></div>
+       <br />
+        <div className="col-lg-8 col-md-8 py-5" 
+        style={{position: "relative", top: "5rem", width: "850px",}}>
+        <h3 
+        style={{fontFamily: "Space Grotesk, sans-serif", fontSize: "38px"}}>
+          Built for the future, designed for peace of mind.
+        </h3>
+             <p style={{fontFamily: "Lato, sans-serif", fontSize: "23px", padding: "10px"}}>CIP is pioneering a secure, tamper-proof crypto inheritance 
+              protocol that ensures your assets are protected for generations
+              to come. With cutting-edge infrastructure, it guarantees that 
+              your digital wealth is safe, sustainable, and future-proof.
+            </p>
+        </div>
+        </div>
         </div>
       </section>
+
+      <section id='new-sec'>
+        <div className="container">
+        <div className="row" id='sec2'>
+          <div className="col-lg-6 col-md-6">
+            <h3>Ready to Secure Your Tomorrow? Here is How It Works:</h3>
+          </div>
+                <div className="col-lg-6 col-md-6 col-sm-12">
+                <ul>
+                   <li>
+                <p>Register your assets on our secure platform.</p>
+            </li>
+         <li>
+           <p>Let AI craft a custom inheritance plan tailored to your needs.</p>
+              </li>
+                  <li>
+                     <p>Smart contracts ensure your assets are transferred when life
+                        events occur.</p>
+                          </li>
+                        <li>
+                     <p>Legal templates validate the process to prevent disputes.</p>
+                 </li>
+              </ul>
+          </div>
+         </div>
+        </div>
+      </section>
+      </div>
     </>
   )
 }
